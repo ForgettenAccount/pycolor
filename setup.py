afd756a0e0820e9e26aa7125e90c955f987b517d
ForgettenAccount <98701076+ForgettenAccount@users.noreply.github.com>
2022-09-07 12:00:39 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="pycolor",
+      version="1.2.1",
+      description="A 16-bit coloring in terminal using Python",
+      author="DuckTech",
+      packages=["pycolor"],
+      zip_safe=False)
